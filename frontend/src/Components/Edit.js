4f73d1fee78e1a198e9d8c6ff9bc6a02a45c5f97
Hobylicious <justiceguy88@gmail.com>
2022-07-13 23:56:14 -0400

    edit now works

--- frontend/src/Components/Edit.js
+++ frontend/src/Components/Edit.js
@@ -1,9 +1,12 @@
 import React, { useState, useEffect } from 'react'
-import { useParams } from 'react-router-dom'
+import { useParams, useNavigate } from 'react-router-dom'
 import axios from 'axios';
 
+
 function Edit() {
-    const [pokemon, setPokemon] = useState([]);
+    const [pokemon, setPokemon] = useState({});
+
+    const navigate = useNavigate()
 
     let params = useParams();
     const id = params.id;
@@ -15,7 +18,7 @@ function Edit() {
             const baseURL = `http://localhost:3001/pokemon/api/edit`
             const res = await axios({ url, responseType: "json", baseURL });
             console.log(res.data);
-            setPokemon(res.data.pokemons)
+            setPokemon(res.data);
         }
         catch (e) {
             console.log(e);
@@ -26,8 +29,78 @@ function Edit() {
         getPokemon();
     }, []);
 
+    const handleSubmit = async (e) => {
+
+        e.preventDefault();
+        try {
+            // const baseURL = `https://pokemondb117.herokuapp.com/pokemon/api`
+            const url = `/${pokemon._id}`
+            const baseURL = `http://localhost:3001/pokemon/api/edit`
+            console.log(pokemon);
+            const res = await axios({ url, responseType: "json", baseURL, method: 'put', data: pokemon });
+            // console.log(res.data);
+            navigate('/pokemon')
+
+
+        } catch (e) {
+            console.log(e);
+        }
+    };
+
+
+
+    const handleChange = (e, gameName, prop) => {
+        const game = pokemon.games.find((gamer) => gamer.name === gameName)
+        game[prop] = !game[prop];
+        setPokemon({ ...pokemon })
+        console.log(pokemon);
+    }
+
     return (
-        <div>Edit</div>
+        <div className='mainPage'>
+            <div className='container' key={pokemon._id}>
+                <img src={pokemon.image} alt="" />
+                <div>Poké Name: {pokemon.name}</div>
+                <div className='divTable'>
+                    <div className='divTableBody'>
+                        <div className='divTableRow'>
+                            <div className='divTableCell'>Type 1</div>
+                            <div className='divTableCell'>{pokemon.pokeType1}</div>
+                        </div>
+                        <div className='divTableRow'>
+                            <div className='divTableCell'>Type 2</div>
+                            <div className='divTableCell'>{pokemon.pokeType2}</div>
+                        </div>
+
+                        <div className='divTableRow'>
+                            <div className='divTableCell'>Height</div>
+                            <div className='divTableCell'>{" "}{pokemon.height} inches</div>
+                        </div>
+                        <div className='divTableRow'>
+                            <div className='divTableCell'>Weight</div>
+                            <div className='divTableCell'>{" "}{pokemon.weight}lbs</div>
+                        </div>
+                        <form onSubmit={handleSubmit}>
+                            <div>
+                                {pokemon && pokemon.games ? pokemon.games.map((data, index) => {
+                                    return (
+                                        <div key={data.name}>
+                                            <div>Game: {data.name}</div>
+                                            <div>Seen:<input type='checkbox' checked={data['seen']} onChange={(e) => handleChange(e, data.name, 'seen')} /></div>
+                                            <div>Caught:<input type='checkbox' checked={data['caught']} onChange={(e) => handleChange(e, data.name, 'caught')} /></div>
+                                            <div>On Team:<input type='checkbox' checked={data['onTeam']} onChange={(e) => handleChange(e, data.name, 'onTeam')} /></div>
+                                        </div>
+                                    )
+                                }) : null}
+                            </div>
+                            <div>
+                                <button>Submit</button>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div >
     )
 }
 
